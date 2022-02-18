82992e268e6d4d4dcc20427d0bc9bed0a2519661
Daniil007 <94208686+Daniil007@users.noreply.github.com>
2022-02-18 11:55:51 -0500

    Division question Solved

--- src/divisionWithoutOperator.java
+++ src/divisionWithoutOperator.java
@@ -1,2 +1,21 @@
+
 public class divisionWithoutOperator {
+    public static void main(String[] args) {
+
+    }
+
+    public static void divide(int a, int b){
+
+        int count = 1;
+        int c = b;
+
+        if (a>=b){
+            while (b<a){
+                count++;
+                b += c;
+            }
+
+        }
+        System.out.println(count);
+    }
 }
